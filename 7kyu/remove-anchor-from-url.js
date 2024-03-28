d9e3d227193af5f6012b6c3d3ff08fdb8ab3327b
Emily Atkins <emily.atkins922@gmail.com>
2024-03-28 15:18:50 -0400

    added new codewars challenge

---
+++ 7kyu/remove-anchor-from-url.js
@@ -0,0 +1,11 @@
+//Instructions
+/* Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1" */
+
+//Solution
+function removeUrlAnchor(url){
+    return url.split('#')[0];
+  }
