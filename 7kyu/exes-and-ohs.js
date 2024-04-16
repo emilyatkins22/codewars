61a93dc5f773a996eceabd8fa2a790c661f7462d
Emily Atkins <emily.atkins922@gmail.com>
2024-04-16 19:00:39 -0400

    added new codewars challenge

---
+++ 7kyu/exes-and-ohs.js
@@ -0,0 +1,25 @@
+//Instructions
+/* Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false */
+
+//Solution
+function XO(str) {
+    let numOfX = 0
+    let numOfO = 0
+    str = str.toLowerCase()
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === 'x') {
+        numOfX++;
+      } else if (str[i] === 'o') {
+        numOfO++;
+      }
+  }
+        return numOfX === numOfO
+  }
