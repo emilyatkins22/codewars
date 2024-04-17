6b43806c7f53ca35a0026eb07ab8e7ea7bfae4c1
Emily Atkins <emily.atkins922@gmail.com>
2024-04-17 10:46:34 -0400

    added new codewars challenge

---
+++ 7kyu/isograms.js
@@ -0,0 +1,27 @@
+//Instructions
+/* An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false */
+
+//Solution
+function isIsogram(str){
+    if (str.isEmpty) {
+      return true
+    } else {
+    str = str.toLowerCase();
+      }
+    let arr = str.split('');
+    arr = arr.slice().sort();
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == arr[i + 1]) {
+        return false
+      }
+    }
+    return true
+  }
