45809fed58c9a0b2caf1a4f05ebe6488e8c99c7d
Emily Atkins <emily.atkins922@gmail.com>
2024-04-26 17:49:47 -0400

    added new codewars challenge

---
+++ 6kyu/replace-with-alphabet-pos.js
@@ -0,0 +1,24 @@
+//Instructions
+/* Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.") */\
+
+//Solution
+function alphabetPosition(text) {
+    let str = text.toLowerCase()
+    let x = ''
+    for (let i = 0; i < str.length; i++) {
+         if (str.charCodeAt(i) - 96 > 0 && str.charCodeAt(i) - 96 <= 26) {
+          x+= str.charCodeAt(i) - 96 + ' ';
+        }
+      }
+      return x.trim();
+    }
+  
