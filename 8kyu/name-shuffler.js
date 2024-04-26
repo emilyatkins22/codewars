9b4b793b1d8939dd503a84eba308988d8e485543
Emily Atkins <emily.atkins922@gmail.com>
2024-04-25 21:30:18 -0400

    added new codewars challenge

---
+++ 8kyu/name-shuffler.js
@@ -0,0 +1,12 @@
+//Instructions
+/* Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john" */
+
+//Solution
+function nameShuffler(str){
+    str = str.split(' ')
+    return str[1] + ' ' + str[0]
+  }
