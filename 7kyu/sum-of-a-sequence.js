cc228633e61ce1a1494c2aeae13c30b0e0195617
Emily Atkins <emily.atkins922@gmail.com>
2024-05-05 21:27:16 -0400

    added new codewars challenge

---
+++ 7kyu/sum-of-a-sequence.js
@@ -0,0 +1,22 @@
+//Instructions
+/* integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step.
+
+If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4) */
+
+//Solution
+const sequenceSum = (begin, end, step) => {
+    let result = 0
+    for (let i = begin; i <= end; i += step) {
+      result += i
+    }
+    return result
+  };
