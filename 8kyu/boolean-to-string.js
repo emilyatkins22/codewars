12a8bbadbcb9a2c827012ec964887f40210de269
Emily Atkins <emily.atkins922@gmail.com>
2024-05-06 20:42:30 -0400

    added new codewars challenge

---
+++ 8kyu/boolean-to-string.js
@@ -0,0 +1,7 @@
+//Instructions
+/* Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given. */
+
+//Solution
+const booleanToString = b => b.toString()
