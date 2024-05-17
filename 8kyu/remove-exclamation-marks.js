60019352783ffcf6e27736008110cb5912e40287
Emily Atkins <emily.atkins922@gmail.com>
2024-05-16 21:01:07 -0400

    added new codewars challenge

---
+++ 8kyu/remove-exclamation-marks.js
@@ -0,0 +1,6 @@
+//Instructions
+/* Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+//Solution
+const removeExclamationMarks = s => s.replace(/!/g, "")
